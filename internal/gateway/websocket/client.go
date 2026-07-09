a5e82c367c2ae66246c750e1ee6fe18eefc7beba
KinMiu <prasetyoaldi875@gmail.com>
2026-07-09 20:32:08 +0700

    update multi device

--- internal/gateway/websocket/client.go
+++ internal/gateway/websocket/client.go
@@ -31,24 +31,35 @@ func NewClient(conn *websocket.Conn, server *Server, role string, id string) *Cl
 func (c *Client) ReadPump() {
 	defer func() {
 		c.server.RemoveDevice(c.ID)
-		log.Printf("[%s] Disconnected (%s)\n", c.ID, c.Role)
+		log.Printf("🔴 [%s] Disconnected (%s)\n", c.ID, c.Role)
 		c.conn.Close()
 	}()
+
+	log.Printf("🔥 [%s] ReadPump mulai berjalan untuk %s\n", c.ID, c.Role)
 
 	for {
 		messageType, payload, err := c.conn.ReadMessage()
 		if err != nil {
+			log.Printf("⚠️ [%s] Error ReadMessage: %v\n", c.ID, err)
 			break
 		}
 
-		// Hanya memproses data biner stream dari Kamera
-		if c.Role == "camera" && messageType == websocket.BinaryMessage {
-			// Channel Redis dinamis: urken:frame:raw:240AC4XXXXXX
-			redisChannel := fmt.Sprintf("urken:frame:raw:%s", c.ID)
+		// DEBUG LOG: Cetak setiap kali ada pesan masuk dari WebSocket
+		log.Printf("📥 [%s] Menerima pesan! Type: %d, Ukuran: %d bytes\n", c.ID, messageType, len(payload))
 
-			err := c.server.Redis.Publish(ctx, redisChannel, payload).Err()
-			if err != nil {
-				log.Printf("Gagal publish ke Redis untuk %s: %v\n", c.ID, err)
+		// Hanya memproses data biner stream dari Kamera
+		if c.Role == "camera" {
+			if messageType == websocket.BinaryMessage {
+				redisChannel := fmt.Sprintf("urken:frame:raw:%s", c.ID)
+
+				err := c.server.Redis.Publish(ctx, redisChannel, payload).Err()
+				if err != nil {
+					log.Printf("❌ Gagal publish ke Redis untuk %s: %v\n", c.ID, err)
+				} else {
+					log.Printf("🚀 Berhasil Publish biner %s ke Redis channel [%s]\n", c.ID, redisChannel)
+				}
+			} else {
+				log.Printf("ℹ️ [%s] Pesan diabaikan karena tipe data bukan Binary (Type: %d)\n", c.ID, messageType)
 			}
 		}
 	}
